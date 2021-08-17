5188d6322fb245c9ebde2048cf51bab80d7b1c58
hassan azarbaijani <hassan.azarbaijani@gmail.com>
2021-08-17 12:15:24 +0430

    weather card update // night png added to assets

---
+++ src/components/weatherCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col  sm="12" md="6" lg="4" align="center">
+        <WeatherSearchInput width="400" />
+      </v-col>
+      <v-col cols="12" align="center">
+        <v-card class="mx-auto" max-width="400">
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5">
+                San Francisco
+              </v-list-item-title>
+              <v-list-item-subtitle
+                >Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-text>
+            <v-row align="center">
+              <v-col class="text-h2" cols="6"> 23&deg;C </v-col>
+              <v-col cols="6">
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+                  alt="Sunny image"
+                  width="92"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-send</v-icon>
+            </v-list-item-icon>
+            <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cloud-download</v-icon>
+            </v-list-item-icon>
+            <v-list-item-subtitle>48%</v-list-item-subtitle>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import WeatherSearchInput from "../components/weatherSearchInput.vue";
+import axios from "axios";
+export default {
+  components: {
+    WeatherSearchInput,
+  },
+  data: () => ({
+    resp: "",
+    search: "qom",
+    token: "c",
+  }),
+  methods: {
+    getData() {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.search}&appid=${this.token}`
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/weatherSearchInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-text-field
+      name="citySearch"
+      label="Weather of what city are you looking for?"
+      solo
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
